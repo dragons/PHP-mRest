b90d682f19c3353c7bbfaef7eb7fecefd79005ef
IUsoh <Isman.usoh@gmail.com>
2012-03-18 19:44:31 +0700

    bbb

---
+++ example/ex1/src/com/isman/test/ex1/Main.java
@@ -0,0 +1,12 @@
+package com.isman.test.ex1;
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ex1");
+	}
+
+}
